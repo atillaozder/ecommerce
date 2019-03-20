12bcf79ebfc28b55cccb08bafa0ef4ed4e8cffec
atungacevik <44293417+atungacevik@users.noreply.github.com>
2019-03-20 11:42:55 +0300

    product models update and access matrix

--- src/apps/product/models.py
+++ src/apps/product/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from django.db.models import Q
 # Create your models here.
 
 class Product(models.Model):
@@ -29,3 +31,55 @@ class Product(models.Model):
     def __str__ (self):
         return self.name
 
+    User = get_user_model()
+
+    class ProductQuerySet(models.query.QuerySet):
+        def active(self):
+            return self.filter(is_active=True)
+
+        def not_active(self):
+            return self.filter(is_active=False)
+
+        def deleted(self):
+            return self.filter(is_deleted=True)
+
+        def not_deleted(self):
+            return self.filter(is_deleted=False)
+
+        def approved(self):
+            return self.filter(is_approved=True)
+
+        def not_approved(self):
+            return self.filter(is_approved=False)
+
+        def accepted(self):
+            return self.active().not_deleted().approved()
+
+        def pending(self):
+            return self.active().not_deleted().not_approved()
+
+        def featured(self):
+            qs_orders = self.by_order_amount(100)
+            qs_likes = self.by_likes(10)
+            qs_discounts = self.accepted().filter(discount__gte=20)
+            return (qs_orders | qs_likes | qs_discounts).distinct()
+
+        def by_order_amount(self, amount, category=None):
+            if category is None:
+                return self.accepted().filter(order_amount__gte=amount)
+            return self.accepted().filter(category_name=category.name).filter(order_amount_gte=amount)
+
+        def by_likes(self, like_amount):
+            return self.accepted().annotate(num_like=('likes')).filter(num_like__gte=like_amount)
+
+        def recent(self):
+            return self.accepted().order_by("-updated", "-timestamp")
+
+        def by_range(self, start_date, end_date=None):
+            if end_date is None:
+                return self.filter(updated__gte=start_date)
+            return self.filter(updated_gte=start_date).filter(updated_lte=end_date)
+
+    class ProductManager(models.Manager):
+        def get_queryset(self):
+            return ProductQuerySet(self.model, using=self._db)
