9d1790c26e772a5dbd4cc16a1567b7ddd817e1da
Metin Berk Karataş <metinberkkaratas@gmail.com>
2019-03-20 12:43:12 +0300

    Category and address fields added

--- src/apps/address/models.py
+++ src/apps/address/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Address(models.Model):
+
+    # country =
+    city = models.CharField(_('City'), max_length=255)
+    address_line = models.TextField(_('Address'), blank=False)
+    notes = models.TextField(_('Notes'), blank=True)
+    postal_code = models.CharField(_('Postal Code'), blank=True, max_length=6)
+    slug = models.SlugField(unique=True, blank=True, max_length=255)
+
+    is_active = models.BooleanField(default=True, verbose_name='Active')
+
+    class Meta:
+        verbose_name = _('Address')
+        verbose_name_plural = _('Addresses')
+
+    def __str__ (self):
+        return self.name
+
--- src/apps/category/models.py
+++ src/apps/category/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(_('Title'), max_length=255)
+    slug = models.SlugField(unique=True, blank=True, max_length=255)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _('Category')
+        verbose_name_plural = _('Categories')
+
+    def __str__ (self):
+        return self.name
