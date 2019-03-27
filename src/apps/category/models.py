85ec3a8252f67a898987a98d1950060eafe87cd8
Metin Berk Karataş <metinberkkaratas@gmail.com>
2019-03-27 12:30:25 +0300

    Address and category modal has been added

--- src/apps/category/models.py
+++ src/apps/category/models.py
@@ -1,18 +1,29 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from utils.utils import unique_slug_generator
+
 
 
 class Category(models.Model):
 
-    name = models.CharField(_('Title'), max_length=255)
+    name = models.CharField(_('Name'), max_length=255)
     slug = models.SlugField(unique=True, blank=True, max_length=255)
 
-    created = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
+        ordering = ['name']
 
     def __str__(self):
         return self.name
+
+@receiver(pre_save, sender=Category)
+def category_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
