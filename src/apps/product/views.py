b7917523171123c74fd4c6a0cabac7523107855d
atungacevik <44293417+atungacevik@users.noreply.github.com>
2019-04-01 16:23:21 +0300

    ProductUpdateView and ProductRejectView created
    ProductUpdateView and ProductRejectView created

--- src/apps/product/views.py
+++ src/apps/product/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponseRedirect, Http404
 from django.core.exceptions import ValidationError
 from .models import Product, ProductLike, ProductImage
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .forms import ProductForm
 from django.views.generic import (
     View,
@@ -19,13 +19,14 @@ from django.views.generic import (
 
 # Create your views here.
 
-class ProductDetailView(DetailView) :
+
+class ProductDetailView(DetailView):
     model = Product
     template_name = 'product_detail.html'
     context_object_name = 'product'
 
     def get_context_data(self, **kwargs):
-        context = super(ProductDetailView,self).get_context_data(**kwargs)
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
         instance = context ['product']
         category = instance.category.all().first()
         products = Product.objects.exclude(pk=instance.pk).by_order_amount(250, category=category)
@@ -42,8 +43,8 @@ def _handle_product_form(request, product, images=None):
         qs_exists = user.distributor.brands.all().filter(pk=product.brand.pk).exists()
         if not qs_exists and not user.is_staff:
             raise ValidationError(_('Please select a brand that you distribute: %(value)s'),
-                                code='invalid',
-                                params={'value': product.brand},)
+                                  code='invalid',
+                                  params={'value': product.brand},)
 
     queryset = ProductImage.objects.all().filter(product=product)
     if queryset.exists():
@@ -65,9 +66,9 @@ class ProductCreateView (LoginRequiredMixin,CreateView):
 
     def form_valid(self, form):
         valid = super(ProductCreateView,self).form_valid(form)
-        images =self.request.FILES.getlist("images")
+        images = self.request.FILES.getlist("images")
 
-        if valid :
+        if valid:
             _handle_product_form(self.request, form.instance,images = images)
             if self.request.user.is_staff:
                 form.instance.is_approved = True
@@ -88,3 +89,62 @@ class ProductApprovePendingListView(LoginRequiredMixin, ListView):
             raise Http404
 
 
+class ProductApproveView(LoginRequiredMixin, View):
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            info_message = _('You are not allowed for this action. Please sign in as ADMIN.')
+        else:
+            slug = self.kwargs.get("slug")
+            instance = get_object_or_404(Product, slug=slug)
+            instance.is_approved = True
+            instance.save()
+            info_message = _('Product has been approved successfully.')
+
+        messages.add_message(request, messages.INFO, info_message)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+class ProductRejectView(LoginRequiredMixin, View):
+
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            info_message = _('Product has been approved successfully.')
+        else:
+            slug = self.kwargs.get("slug")
+            instance = get_object_or_404(Product, slug=slug)
+            instance.is_active = False
+            instance.save()
+            info_message = _('Product has been rejected successfully.')
+
+        messages.add_message(request, messages.INFO, info_message)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    template_name = "product_form.html"
+    form_class = ProductForm
+
+    def get_object(self):
+        user = self.request.user
+        slug = self.kwargs.get("slug")
+        instance = get_object_or_404(Product, slug=slug)
+        if user.user_type == 'distributor':
+            if instance.is_active and not instance.is_deleted:
+                return instance
+            else:
+                raise Http404
+        elif user.is_staff:
+            return instance
+        else:
+            raise Http404
+
+    def form_valid(self, form):
+        valid = super(ProductUpdateView, self).form_valid(form)
+        images = self.request.FILES.getlist("images")
+        if valid:
+            _handle_product_form(self.request, form.instance, images=images)
+        return valid
+
+#class ProductDeleteView(LoginRequiredMixin,View):
